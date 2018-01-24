0e8d6f4d0866b387bf8df2621a0af0d3cafb6859
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-24 12:42:58 +0530

    fixing repoUrl

--- slappbooks/slappbooks-lambda.js
+++ slappbooks/slappbooks-lambda.js
@@ -1,7 +1,9 @@
 let AWS = require('aws-sdk');
 let connectionManager = require('./ConnectionManager');
 let SL = require('@slappforge/slappforge-sdk');
+
 const rds = new SL.AWS.RDS(connectionManager);
+
 exports.handler = function (event, context, callback) {
 	console.log(event);
 	let transactions = [];
